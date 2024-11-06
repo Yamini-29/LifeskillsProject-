d09c4b38f563922c51016b246fa27ed747a2d5f7
Jai <jjaipradeep@gmail.com>
2024-11-06 10:19:59 +0530

    addproduct page

--- components/AdminComponents/Sidebar.jsx
+++ components/AdminComponents/Sidebar.jsx
@@ -1,20 +1,32 @@
-import { assets } from '@/assets/assets'
-import React from 'react'
-import Image from 'next/image'
+import { assets } from "@/assets/assets";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Sidebar = () => {
   return (
-    <div className='flex flex-col bg-slate-100'>
-      <div className='px-2 sm:pl-14 py-3 border border-black'>
-        ADMIN
+    <div className="flex flex-col bg-slate-100">
+      <div className="px-2 sm:pl-14 py-3 border border-black">
+        <Image src={assets.logo} width={120} alt="" />
       </div>
-      <div className='w-28 sm:w-80 h-[100vh] relative py-12 border border-black'>
-        <div className='flex items-center border border-black gap-3 font-medium px-3 py-2 bg-white shadow-[-5px_5px_0px_#000000]'>
-        <Image src={assets.add_icon}  width={28} /><p>Add Courses</p>
+      <div className="w-28 sm:w-80 h-[100vh] relative py-12 border border-black">
+        <div className="w-[50%] sm:w-[80%] absolute right-0">
+          <Link href='/admin/addCourse' className="flex items-center border border-black gap-3 font-medium px-3 py-2 bg-white shadow-[-5px_5px_0px_#000000]">
+            <Image src={assets.add_icon} alt="" width={28} />
+            <p>Add Courses</p>
+          </Link>
+          <Link href='/admin/courseList' className="flex mt-5 items-center border border-black gap-3 font-medium px-3 py-2 bg-white shadow-[-5px_5px_0px_#000000]">
+            <Image src={assets.blog_icon} alt="" width={28} />
+            <p>Course List</p>
+          </Link>
+          <Link href='/admin/registrations' className="flex mt-5 items-center border border-black gap-3 font-medium px-3 py-2 bg-white shadow-[-5px_5px_0px_#000000]">
+            <Image src={assets.arrow} alt="" width={28} />
+            <p>Registrations</p>
+          </Link>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
